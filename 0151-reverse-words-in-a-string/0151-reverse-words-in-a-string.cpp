07a9df21f0228b652e6c909731ec57c4e8f0ce93
vivekrai05-sudo <vivek01234bxr@gmail.com>
2026-04-12 01:18:32 +0530

    Time: 0 ms (100%), Space: 11.3 MB (19.59%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        vector<string> words;
+        string temp = "";
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == ' ') {
+                if (temp != "") {
+                    words.push_back(temp);
+                    temp = "";
+                }
+            } else {
+                temp += s[i];
+            }
+        }
+
+        if (temp != "") words.push_back(temp);
+
+        reverse(words.begin(), words.end());
+
+        string ans = "";
+        for (int i = 0; i < words.size(); i++) {
+            ans += words[i];
+            if (i != words.size() - 1) ans += " ";
+        }
+
+        return ans;
+    }
+};
