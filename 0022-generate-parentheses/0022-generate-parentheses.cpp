267fbb0fb52f92b14dbc554e3a73532d2b31af2e
vivek kumar rai <vivek01234bxr@gmail.com>
2026-05-24 00:01:42 +0530

    Time: 4 ms (31.65%), Space: 16.4 MB (11.74%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
 
-    void solve(vector<string> &ans,int n,int open,int close,string output){
+    void solve(vector<string> &ans,int n,int open,int close,int rem_open,int rem_close,string output){
     //BASE CASE
-    if(open== 0 && close == 0){
+    if(rem_open== 0 && rem_close == 0){
         ans.push_back(output);
         return ;
     }
 
    //include open bracket
-   if(open>0){
+   if(rem_open>0){
     output.push_back('(');
-    solve(ans,n,open-1,close,output);
+    solve(ans,n,open+1,close,rem_open-1,rem_close,output);
     //backtracking
     output.pop_back();
    }
    //include close bracket
-   if(close>open){
+   if(open>close){
     output.push_back(')');
-    solve(ans,n,open,close-1,output);
+    solve(ans,n,open,close+1,rem_open,rem_close-1,output);
     //backtrack
     output.pop_back();
    }
@@ -26,10 +26,12 @@ public:
 
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        int open=n;
-        int close=n;
+        int open=0;
+        int close=0;
+        int rem_open=n;
+        int rem_close=n;
         string output="";
-        solve(ans,n,open,close,output);
+        solve(ans,n,open,close,rem_open,rem_close,output);
         return ans;
     }
 };
