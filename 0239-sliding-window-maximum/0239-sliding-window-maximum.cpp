08941b44ed1d903e5a92299bc88c3edac71542e3
vivek kumar rai <vivek01234bxr@gmail.com>
2026-08-19 00:52:03 +0530

    Time: 16 ms (85.22%), Space: 139.3 MB (49.07%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            // Remove elements outside the window
+            if (!dq.empty() && dq.front() <= i - k) {
+                dq.pop_front();
+            }
+
+            // Remove smaller elements from the back
+            while (!dq.empty() && nums[dq.back()] < nums[i]) {
+                dq.pop_back();
+            }
+
+            // Add current index
+            dq.push_back(i);
+
+            // Start adding answers when window size becomes k
+            if (i >= k - 1) {
+                ans.push_back(nums[dq.front()]);
+            }
+        }
+
+        return ans;
+    }
+};
