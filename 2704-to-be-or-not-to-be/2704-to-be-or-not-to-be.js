d21db57bc3a62f63e4398ff7b230689954081621
vivek kumar rai <vivek01234bxr@gmail.com>
2026-09-11 14:13:07 +0530

    Time: 43 ms (59.82%), Space: 53.1 MB (75.94%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return{
+        toBe:function(val2){
+            if(val===val2)
+            return true;
+            else
+            throw Error("Not Equal");
+        },
+        notToBe:function(val2){
+            if(val!==val2)
+            return true;
+            else
+            throw Error("Equal");
+        }
+    }
+};
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
