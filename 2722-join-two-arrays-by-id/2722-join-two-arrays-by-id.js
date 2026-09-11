e9985c43c79056ec3b94452c6476c9e7661b21c1
vivek kumar rai <vivek01234bxr@gmail.com>
2026-09-11 14:10:13 +0530

    Time: 317 ms (33.89%), Space: 103.7 MB (32.31%) - LeetHub

---
+++ 2722-join-two-arrays-by-id/2722-join-two-arrays-by-id.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+var join = function(arr1, arr2) {
+    const map=new Map();
+     for (let obj of arr1) {
+        map.set(obj.id, obj);
+    }
+    for (let obj of arr2) {
+        if (map.has(obj.id)) {
+            map.set(obj.id, { ...map.get(obj.id), ...obj });
+        } else {
+            map.set(obj.id, obj);
+        }
+    }
+    return [...map.values()].sort((a, b) => a.id - b.id);
+};
